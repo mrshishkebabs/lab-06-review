9adece3e8447e3c8fe8b2b0fe8e1f7dc25cdd2eb
Ken Park <xkenxparkx@gmail.com>
2021-10-18 16:34:37 -0400

    Temperature on both HTML and PYTHON versios

---
+++ temperature.py
@@ -0,0 +1,8 @@
+﻿tempincelsius = input("Please enter a temperature in Celsius... ")
+tempincelsius = float(tempincelsius)
+# tempincelsius = float(input("Please enter a temperature in Celsius")) <- Alternate solution
+# print(type(tempincelsius))
+tempinfahrenheit = tempincelsius * 1.8 + 32
+tempinfahrenheit = round(tempinfahrenheit, 2)
+    # round() is a FUNCTION used to limit the number of decimal places
+print(str(tempincelsius) + " degrees Celsius is " + str(tempinfahrenheit) + " degrees Fahrenheit")
